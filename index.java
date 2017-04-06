6195f0c7d5770f07866cb3fb679ace32eae02f8a
ernestinanavarro <navarro.ernestina99@gmail.com>
2017-04-05 23:24:01 -0700

    Update index.java

--- index.java
+++ index.java
@@ -21,31 +21,36 @@ angular.module('app', ['cfp.hotkeys', 'ngAnimate'])
                 'rank' : 1,
             	'name' : 'Debate Club',
                 'desc' : 'Enjoy having a good arguement or proving people wrong, then take part of this club. You will learn how to have an effective debate and learn how to improve on yout speaking skills. ',
-                'img' : 'http://3.bp.blogspot.com/-fcr_XPbP8_g/V8pSvMu0RjI/AAAAAAAADwk/fSI93vc4JWcOw--jiIq6JjQWf1hOokQ7wCLcB/s1600/debate.png'
+                'location':'husday from 4-5:00pm in Scott's Room',
+		'img' : 'http://3.bp.blogspot.com/-fcr_XPbP8_g/V8pSvMu0RjI/AAAAAAAADwk/fSI93vc4JWcOw--jiIq6JjQWf1hOokQ7wCLcB/s1600/debate.png'
         	},
             {
                 'rank' : 2,
             	'name' : 'Anime Club',
-                'desc' : '',
-                'img' : 'https://s-media-cache-ak0.pinimg.com/originals/05/c5/70/05c57019bc873a9cbe3cf7d4136f0299.jpg'
+                'desc' : ' SAO,Black Butler and Attack on Titan. Come and join this club to learn more and discuss about anime',
+                'location':'Every other Monday from 4-5:00pm in Itow's Rm ',
+		'img' : 'https://s-media-cache-ak0.pinimg.com/originals/05/c5/70/05c57019bc873a9cbe3cf7d4136f0299.jpg'
         	},
             {
                 'rank' : 3,
             	'name' : 'Archery Club',
                 'desc' : 'You will learn how to participate in Archery. The fundamentals of archery and how to use and care for equipment, basic safety, techniques and fundamentals of shooting but best of all have fun.'
-                'img' : 'http://www.roundtopswim.com/wp-content/uploads/2015/02/archerytargets1.jpg'
+                'location':'Tuesday and Thursday from 4-6:00 pm in the feild',
+		'img' : 'http://www.roundtopswim.com/wp-content/uploads/2015/02/archerytargets1.jpg'
         	},
             {
                 'rank' : 4,
             	'name' : 'Vocals 101',
-                'desc' : '',
-                'img' : 'https://blog.oup.com/wp-content/uploads/2015/07/1260-microphone.jpg'
+                'desc' : 'Are you the one who sings along to every song on the radio. Come and learn how to become a better singer and sing with those singing lovers',
+                'location':'Monday and Thursday from 8-9:00 am in Cari's rm',
+		'img' : 'https://blog.oup.com/wp-content/uploads/2015/07/1260-microphone.jpg'
         	},
             {
                 'rank' : 5,
             	'name' : 'Girl Code',
                 'desc' : 'Here girls who have a passion for code will come together. You will work on special projects to help you learn more with using different programming launguages.',
-                'img' : 'https://media.licdn.com/media/p/8/005/08a/35c/274939f.png'
+                'location':'Every other Monday from 4:10-5:00 pm in Rm.205',
+		'img' : 'https://media.licdn.com/media/p/8/005/08a/35c/274939f.png'
         	}
         ];
     
